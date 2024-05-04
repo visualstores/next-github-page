a4f1ba5f2d0553b85ce4dbea5615d5fad88214eb
ojpfight <ojpfight@gmail.com>
2024-05-04 16:35:23 +0900

    링크 추가

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function About() {
-    return <div>About</div>
+  return (
+    <>
+      <h3>About</h3>
+      <Link href="/">Home</Link>
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,9 @@ export default function Home() {
             Get started by editing&nbsp;
             <code className={styles.code}>src/pages/index.tsx</code>
           </p>
+          <div>
+            <Link href="/about">About</Link>
+          </div>
           <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
