b9bd553e3edcc9d7730c5f7c7c7b66b934d82787
ojpfight <ojpfight@gmail.com>
2024-05-04 16:43:12 +0900

    about : 리스트 버튼 추가

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,10 +1,27 @@
 import Link from "next/link";
+import { useState } from "react";
 
 export default function About() {
+  const [list, setList] = useState([] as string[]);
+
+  const onSearch = () => {
+    let tempList: string[] = [];
+
+    for (let i = 0; i < 10; i++) {
+      tempList.push(`사용자 ${i}`);
+    }
+
+    setList(tempList);
+  };
   return (
     <>
       <h3>About</h3>
       <Link href="/">Home</Link>
+      <hr></hr>
+      <button onClick={onSearch}>리스트</button>
+      {list.map((item) => (
+        <div key={item}>{item}</div>
+      ))}
     </>
   );
 }
